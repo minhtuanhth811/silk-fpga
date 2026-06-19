73cb28f85acd6f294e9b1c03b3ebc79a05f26d4a
Đào Minh Tuấn <minhtuanhth811@gmail.com>
2026-06-19 19:41:15 +0700

    my_vga_project_v1.1.3 - fixbug

--- remote_lab/your_project/palette.v
+++ remote_lab/your_project/palette.v
@@ -5,15 +5,17 @@ module palette(
   output wire [5:0] rrggbb
 );
   reg [5:0] palette[7:0];
-  
-  palette[0] = 6'b00_00_00; //black
-  palette[1] = 6'b00_00_11; //blue
-  palette[2] = 6'b00_11_00; //green
-  palette[3] = 6'b11_00_00; //red
-  palette[4] = 6'b00_11_11; //cyan
-  palette[5] = 6'b11_00_11; //purple
-  palette[6] = 6'b11_11_00; //yellow
-  palette[7] = 6'b11_11_11; //white
+  initial
+    begin
+      palette[0] = 6'b00_00_00; //black
+      palette[1] = 6'b00_00_11; //blue
+      palette[2] = 6'b00_11_00; //green
+      palette[3] = 6'b11_00_00; //red
+      palette[4] = 6'b00_11_11; //cyan
+      palette[5] = 6'b11_00_11; //purple
+      palette[6] = 6'b11_11_00; //yellow
+      palette[7] = 6'b11_11_11; //white
+    end
 
   assign rrggbb = palette[color_index];
   
