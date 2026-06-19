f383a7e61db4a325a588fb0ef3d5478272a9df06
Đào Minh Tuấn <minhtuanhth811@gmail.com>
2026-06-19 19:31:40 +0700

    my_vga_project_v1.1.0 - add_palette

---
+++ remote_lab/your_project/palette.v
@@ -0,0 +1,20 @@
+`default_nettype none
+
+module palette(
+  input wire [2:0] color_index,
+  output wire [5:0] rrggbb
+)
+  reg [5:0] palette [7:0];
+  
+  palette[0] = 6'b00_00_00; //black
+  palette[1] = 6'b00_00_11; //blue
+  palette[2] = 6'b00_11_00; //green
+  palette[3] = 6'b11_00_00; //red
+  palette[4] = 6'b00_11_11; //cyan
+  palette[5] = 6'b11_00_11; //purple
+  palette[6] = 6'b11_11_00; //yellow
+  palette[7] = 6'b11_11_11; //white
+
+  assign rrggbb = palette[color_index];
+  
+endmodule
